5dd8f02c63d42f44689f3c969fb04f9029353907
Karan <karansharma192004@gmail.com>
2024-10-17 21:03:42 +0530

    stopwatch-react

--- week-9-react-practice/9.2-react/src/App.jsx
+++ week-9-react-practice/9.2-react/src/App.jsx
@@ -2,22 +2,34 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  return(
-<div style={{backgroundColor:"#dfe6e9", height:"100vh"}}>
-<PostComponent></PostComponent>
-</div>
-)
-}
-function PostComponent(){
-  return <div style={{alignItems:'center', gap:10, width:400, backgroundColor:"white", borderRadius:10, borderColor:"gray", borderWidth:1, padding:"20px"}}><div style={{display:"flex", gap:10}}>
-    <img src="https://storage.googleapis.com/fc-freepik-pro-rev1-eu-static/ai-styles-landings/anime/characters-and-scenes.jpg?h=1280" alt="" style={{width:40, height:40, borderRadius:20}}/>
-  <div>
-    <b>100xdevs</b>
-    <div>23,800 followers</div>
-    <div>12m</div>
-  </div>
- 
-  </div>
-   <div style={{padding:"10px"}}>Explore Sarcaeus's board "Anime Profile Pictures", followed by 499 people on Pinterest.</div></div>
+  const [count, setcount] = useState(0);
+  const [start, setstart] = useState(false);
+
+  function countHandler(){
+     {start? setcount(count => count+1): setcount(0)}
+  }
+
+  useEffect(function(){
+    console.log("above interval")
+    const interval = setInterval(countHandler, 1000);
+
+    return () => clearInterval(interval)
+  },[start])
+
+
+  function startWatch(){
+    setstart(true)
+  }
+  function stopWatch(){
+    setstart(false)
+  }
+
+  return (
+    <div style={{display:"flex", alignItems:"center", gap:5}}>
+     {start? <div>{count}</div>:null}
+      <button onClick={startWatch}>START</button>
+      <button onClick={stopWatch}>STOP</button>
+    </div>
+ )
 }
 export default App
