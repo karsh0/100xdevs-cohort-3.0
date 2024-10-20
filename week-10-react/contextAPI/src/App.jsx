73ba30852873ac7efd66a9fb42256472b55385b3
Karan <karansharma192004@gmail.com>
2024-10-21 00:11:32 +0530

    Context Api

--- week-10-react/contextAPI/src/App.jsx
+++ week-10-react/contextAPI/src/App.jsx
@@ -1,45 +1,38 @@
 
-import { useState } from 'react'
-import './App.css'
+import { createContext, useContext, useState } from 'react'
+
+//increase decrease count:1
+const CounterContext = createContext();
 
 function App() {
-  const [bulbOn, setbulbOn] = useState(true)
-  return (
-    <>
-    {/* //creating a bulb toggler */}
-    <Light bulbOn={bulbOn} setbulbOn={setbulbOn}/>
+  const [count , setCount] = useState(0);
 
-    </>
+
+  return (
+    <CounterContext.Provider value={{count,setCount}}>
+    <Increase/>
+    <Decrease/>  
+    <Counter/>
+    </CounterContext.Provider>
+    
   )
 }
 
-function Light({bulbOn,setbulbOn}){
-  return (
-  <div>
-    <Bulb bulbOn={bulbOn}/>
-    <BulbToggler bulbOn={bulbOn} setbulbOn={setbulbOn}/>
-  </div>
-  )
+
+function Increase(){
+  const {setCount} = useContext(CounterContext);
+  return <button onClick={()=> setCount(count => count+1)}>Increase</button>
 }
 
-function Bulb({bulbOn}){
-  return (
-    <div>
-      {bulbOn?"Bulb is On":"Bulb is Off"}
-    </div>
-  )
+function Decrease(){
+  const {setCount} = useContext(CounterContext);
+  return <button onClick={()=> setCount(count => count-1)}>Decrease</button>
+
 }
 
-function BulbToggler({bulbOn, setbulbOn}){
-
-  function toggler(){
-    setbulbOn(!bulbOn)
-  }
-
-  return (
-    <button onClick={toggler}>Toggle the bulb</button>
-  )
+function Counter(){
+  const {count} = useContext(CounterContext);
+  return <p>Count is: {count}</p>
 }
-
 
 export default App
