9c0156b00b4647d1f1658790d8018a39cc21a863
Karan <karansharma192004@gmail.com>
2024-10-20 14:39:27 +0530

    route in react

--- week-10-react/vite-project/src/App.jsx
+++ week-10-react/vite-project/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <BrowserRouter>
+    <div style={{display:"flex", width:"300px", justifyContent:"space-between"}}>
+    <Link to="/">Home</Link>
+    <Link to="/neet/online-coaching-class-11">Class 11</Link>
+    <Link to="/neet/online-coaching-class-12">Class 12</Link>
+    <Link to="/neet/online-coaching-class-undergrad">Undergrads</Link>
+    </div>
+
+    <Routes>
+      <Route path="/neet/online-coaching-class-11" element={<Class11Program/>}></Route>
+      <Route path="/neet/online-coaching-class-12" element={<Class12Program/>}></Route>
+      <Route path="/neet/online-coaching-class-undergrad" element={<UndergradProgram/>}></Route>
+      <Route path="/" element={<Landing/>}></Route>
+    </Routes>
+    
+    </BrowserRouter>
+     
+    </div>
   )
 }
 
+function Class11Program(){
+  return <div>Coachinng for class 11</div>
+}
+
+function Class12Program(){
+  return <div>Coachinng for class 12</div>
+}
+
+function UndergradProgram(){
+  return <div>Coachinng for undergrads</div>
+}
+ 
+
+function Landing(){
+  return <div>Allen classes</div>
+}
+ 
 export default App
