84d721d91baf9030954736444518de34a690c2a7
Karan <karansharma192004@gmail.com>
2024-10-27 21:33:25 +0530

    memo

---
+++ week-11-live/Recoil/src/App.jsx
@@ -0,0 +1,70 @@
+
+import { createContext } from 'react';
+import './App.css'
+import { useContext } from 'react';
+import { useState } from 'react';
+import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from 'recoil'
+import { counterAtom } from './store/atoms/counter';
+
+function App() {
+//creating a increase/decrease functionality
+  return (
+    <>
+      <RecoilRoot>
+        <Counter/>
+      </RecoilRoot>
+    </>
+  )
+}
+
+//selectors
+
+
+
+// const countContext = createContext();
+
+// function CounterContextProvider({children}){
+//   const [count, setcount] = useState(0);
+
+//   return (
+//     <countContext.Provider value={{count, setcount}}>{children}</countContext.Provider>
+//   )
+// }
+
+function Increase(){
+  const setcount = useSetRecoilState(counterAtom);
+
+const IncreaseFn = () =>{
+  setcount(c => c+1);
+}
+
+  return <div>
+    <button onClick={IncreaseFn}>Increase</button>
+  </div>
+}
+function Decrease(){
+  const setcount = useSetRecoilState(counterAtom);
+
+const DecreaseFn = () =>{
+  setcount(c => c-1);
+}
+
+  return <div>
+    <button onClick={DecreaseFn}>Decrease</button>
+  </div>
+}
+const CurrentCounter = () =>{
+  const count = useRecoilValue(counterAtom);
+  return <div>{count}</div>
+}
+function Counter(){
+  return (
+    <div>
+    <CurrentCounter/>
+    <Increase/>
+    <Decrease/>
+    </div>
+  )
+}
+
+export default App
