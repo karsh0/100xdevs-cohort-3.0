43a50994bd2f510e2e9699a1c6ad2fa09c767d5b
Karan <karansharma192004@gmail.com>
2024-11-26 20:40:45 +0530

    chat using rooms

--- week-16-websockets/chat/src/index.ts
+++ week-16-websockets/chat/src/index.ts
@@ -2,17 +2,30 @@ import {WebSocketServer, WebSocket} from "ws"
 
 const wss = new WebSocketServer({port: 8080});
 
-let userCount = 0;
-const allSockets: WebSocket[] = [];
+interface User {
+    socket: WebSocket,
+    roomId: String
+}
+
+const allSockets: User[] = [];
 
 wss.on("connection", function(socket){
-    console.log("user connected")
-    userCount = userCount + 1;
-    allSockets.push(socket)
     socket.on("message", (message)=>{
-            allSockets.forEach((s) =>{
-            s.send(message + ": server sent")
-        })
+        let parsedMessage = JSON.parse(message as unknown as string);
+
+        if(parsedMessage.type === "join"){
+            allSockets.push({
+                socket,
+                roomId: parsedMessage.payload.roomId
+            })
+        }
+        if(parsedMessage.type === "chat"){
+            const user = allSockets.find((u) => u.socket == socket);
+            
+            const rooms = allSockets.filter((u) => u.roomId == user?.roomId)
+            rooms.map((room)=> room.socket.send(parsedMessage.payload.message))
+        }
+
     })
-    console.log("user connected + ", userCount)
+
 })
