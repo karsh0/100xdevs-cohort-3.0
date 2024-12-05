95083d68a30d207e45227285ec6d6ab59112dbd7
Karan <karansharma192004@gmail.com>
2024-12-05 17:55:35 +0530

    Insert query

--- week-17-Postgresql/src/index.ts
+++ week-17-Postgresql/src/index.ts
@@ -1,11 +1,36 @@
 import { Client } from 'pg'
+import express from 'express'
 
 const pgClient = new Client('postgresql://neondb_owner:7QKFJm5ShyqE@ep-black-shadow-a5za8vx4.us-east-2.aws.neon.tech/neondb?sslmode=require')
+const app = express()
+app.use(express.json())
+pgClient.connect()
 
-async function main(){
-    await pgClient.connect()
-    const response = await pgClient.query("SELECT * FROM users")
-    console.log(response)
-}
+app.post('/signup', async(req,res)=>{
+    const {username, password, email} = req.body;
 
-main()
+    try{
+        const response = await pgClient.query(`INSERT INTO users (username, password, email) VALUES ('${username}', '${password}', '${email}')`)
+        console.log(response)
+        res.json({
+            message:"user signed up"
+        })
+    }catch(e){
+        res.json({
+            message:"unable to query"
+        })
+        console.log(e)
+    }
+})
+app.listen(3000)
+
+
+
+
+// async function main(){
+//     await pgClient.connect()
+//     const response = await pgClient.query(`UPDATE FROM user(username, password, email) VALUES ('${username}', '${password}', '${email}')`)
+//     console.log(response)
+// }
+
+// main()
