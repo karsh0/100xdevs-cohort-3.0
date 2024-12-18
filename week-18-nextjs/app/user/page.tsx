7c1f783e8fb9f2ac69a1a82452cc32ee17db1cdf
Karan <karansharma192004@gmail.com>
2024-12-18 19:21:01 +0530

    test

---
+++ week-18-nextjs/app/user/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+export default function User(){
+    const [data, setdata] = useState();
+    const [loading, setloading] = useState(true);
+
+    useEffect(()=>{
+        axios.get('https://week-13-offline.kirattechnologoes.workers.dev/api/v1/user/details').then(response =>{
+            setdata(data)
+            setloading(false)
+        })
+       
+    },[])
+    
+    if(loading){
+        return <div>Loading...</div>
+    }
+
+    return <div>
+        {data.name}
+    </div>
+}
